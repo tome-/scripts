7a15f2372e4f1d82eb82c471899d0b4b650d0c71
grimi <grimi@poczta.fm>
2015-04-15 08:24:23 +0200

    2015-04-15 08:24

--- bin/shterm.sh
+++ bin/shterm.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Name:    shterm
 # About:   run program in terminal
 # Author:  grimi <grimi at poczta dot fm>
@@ -8,66 +8,67 @@
 usage() {
   echo "USAGE: $(basename $0) [options] <command> [filearg] [args...]"
   echo "Options:
-        -g <geometry>    terminal geometry WxH
-        -a <cmd & args>  run after main prog exit
-        -w               don't close term, wait for hit Enter
-        -n               no terminal (and ignore term specific options)
-        -d               debug mode (don't run, just show cmd line)"
-
+      -g <geometry>    terminal geometry WxH
+      -a <cmd & args>  run after main prog exit
+      -w               don't close term, wait for hit Enter
+      -n               no terminal (and ignore term specific options)
+      -d               dry mode (don't run, just show cmd line)"
   exit 1
 }
 
-[ -z "$1" ] && usage
+[[ -z $1 ]] && usage
 
 while getopts "a:g:wnd" opt; do
-   case $opt in
-      g) geom="-g $OPTARG";;
-      a) after="$OPTARG";;
-      w) wait4="echo -e '\n::: Enter :::' ; read";;
-      n) noterm=1;;
-      d) debug=1;;
-   esac
+  case $opt in
+    g) geom="-g $OPTARG";;
+    a) after="$OPTARG";;
+    w) wait4="echo -e '\n::: Enter :::' ; read";;
+    n) noterm=1;;
+    d) dry=1;;
+  esac
 done
 
 shift $((OPTIND-1)); OPTIND=1
 
-[[ -z "$1" ]] && usage
+[[ -z $1 ]] && usage
 
 app="$1" && shift
 
 cd "$(dirname "$app")"
 
-if [[ -n "${1%-*}" ]]; then
-   cd "$(dirname "$1")"
-   arg="$(basename "$app")"
-   [[ -x "$arg" ]] && app="./$arg"
-   arg="$(basename "$1")"
-   title="$app $(basename "$1")"
-   shift
+if [[ -n ${1%-*} ]]; then
+  cd "$(dirname "$1")"
+  arg="$(basename "$app")"
+  [[ -x $arg ]] && app="./$arg"
+  arg="$(basename "$1")"
+  title="$app $(basename "$1")"
+  shift
 else
-   title="$app"
+  title="$app"
 fi
 
 
 cmdline="\"$app\""
-[[ -n "$arg" ]] && cmdline+=" \"$arg\""
-while [[ -n "$1" ]]; do
-   cmdline+=" \"$1\""
-   shift
+[[ -n $arg ]] && cmdline+=" \"$arg\""
+while [[ -n $1 ]]; do
+  cmdline+=" \"$1\""
+  shift
 done
 
-[[ -n "$after" ]] && cmdline+=" ; eval '$after'"
+[[ -n $after ]] && cmdline+=" ; eval '$after'"
 
-if [ -n "$debug" ]; then
-   echo "$(pwd): $cmdline"
-   exit
+if [[ -n $dry ]]; then
+  echo "$(pwd): $cmdline"
+  exit
 fi
 
 if [[ -z $noterm ]]; then
-  [[ -z "$wait4" ]] && wait4="sleep 5"
+  [[ -z $wait4 ]] && wait4="sleep 5"
   xterm -T "$title" $geom -e "echo ; $cmdline ; $wait4"
 else
   sh -c "$cmdline"
 fi
 
+
+# vim:set ts=2 sw=2 sts=2 et:
 
